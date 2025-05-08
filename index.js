07d8d3acde7f94fb21ab50f2b47d77bacdb13272
zulqarnain54088 <zulqarnainkiani133@gmail.com>
2025-05-08 14:58:58 +0500

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3300;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js Docker App!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
